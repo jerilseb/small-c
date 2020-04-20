0f5785876ade7adc309ee968c696d2955c0fe272
Jeril Sebastian <seb988@gmail.com>
2020-04-20 14:57:26 +0530

    ast struct added

--- defs.h
+++ defs.h
@@ -3,12 +3,6 @@
 #include <string.h>
 #include <ctype.h>
 
-// Token structure
-struct token
-{
-    int token;
-    int intvalue;
-};
 
 // Tokens
 enum
@@ -18,4 +12,30 @@ enum
     T_STAR,
     T_SLASH,
     T_INTLIT
+};
+
+// Token structure
+struct token
+{
+    int token;
+    int intvalue;
+};
+
+// AST node types
+enum
+{
+    A_ADD,
+    A_SUBTRACT,
+    A_MULTIPLY,
+    A_DIVIDE,
+    A_INTLIT
+};
+
+// Abstract Syntax Tree structure
+struct ASTnode
+{
+    int op;               // "Operation" to be performed on this tree
+    struct ASTnode *left; // Left and right child trees
+    struct ASTnode *right;
+    int intvalue; // For A_INTLIT, the integer value
 };
