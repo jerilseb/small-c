ff55a6c25ae18460b47a15bb1c1d84b7f4b53ab7
Jeril Sebastian <seb988@gmail.com>
2021-03-04 18:54:57 +0530

    enum and typedef tests

--- test/input1.c
+++ test/input1.c
@@ -1,14 +1,46 @@
 int printf(char *fmt);
 
-enum fred { apple=1, banana, carrot, pear=10, peach, mango, papaya };
-enum jane { aple=1, bnana, crrot, par=10, pech, mago, paaya };
+enum fred
+{
+    apple = 1,
+    banana,
+    carrot,
+    pear = 10,
+    peach,
+    mango,
+    papaya
+};
+
+enum jane
+{
+    aple = 1,
+    bnana,
+    crrot,
+    par = 10,
+    pech,
+    mago,
+    paaya
+};
 
 enum fred var1;
 enum jane var2;
 enum fred var3;
 
-int main() {
-  var1= carrot + pear + mango;
-  printf("%d\n", var1);
-  return(0);
+typedef int *FOO;
+
+struct bar
+{
+    int x;
+    int y
+};
+
+typedef struct bar *BAR;
+BAR var22;
+
+int main()
+{
+    var1 = carrot + pear + mango;
+    printf("%d\n", var1);
+
+    return (0);
 }
